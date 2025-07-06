9a0104e82fbb9a05bbc67234f4d8f7ee2c302ec7
engRana404 <ranagamaldaif@gmail.com>
2025-07-07 01:48:32 +0300

    Add tests for user authentication and update user details
    - Implemented tests for login functionality, ensuring a 200 status code is returned with valid credentials.
    - Added tests for accessing protected routes with and without a token, verifying appropriate status codes.
    - Included a test for updating user details and deleting a user with valid authentication.

--- api_tests/auth.test.js
+++ api_tests/auth.test.js
@@ -1,6 +1,8 @@
 const supertest = require('supertest');
 const request = supertest('http://localhost:3000');
 
+let token;
+
 beforeAll(async () => {
   await request.delete('/api/v1/all-users').send({"key_admin": "keyadmin123"});
 });
@@ -28,18 +30,60 @@ describe('POST /api/v1/users', () => {
     })
   })
 
+  
   describe('Login with valid credentials', () => {
     test('should respond with a 200 status code', async () => {
       const response = await request.post('/api/v1/auth').send({
         "email": "user@example.com", 
         "password": "user123" 
       })
+      token = response.body.token
+      expect(response.statusCode).toBe(200)
+    })
+  })
+})
+
+describe('GET /api/v1/auth', () => {
+  describe('Access protected route without token', () => {
+    test('should respond with a 401 status code', async () => {
+      const response = await request.get('/api/v1/users')
+      expect(response.statusCode).toBe(401)
+    })
+  })
+  describe('Access protected route with token', () => {
+    test('should respond with a 200 status code', async () => {
+      const response = await request
+        .get('/api/v1/users')
+        .set('Authorization', token)
+      expect(response.statusCode).toBe(200)
+    })
+  })
+})
+
+describe('PATCH /api/v1/users', () => {
+  describe("Update user name", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request
+        .patch('/api/v1/users')
+        .set('Authorization', token)
+        .send({
+          "name": "Updated Name"
+        })
       expect(response.statusCode).toBe(200)
     })
   })
 })
 
 describe('DELETE /api/v1/all-users', () => {
+  describe("Delete user using valid authentication token", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request
+        .delete('/api/v1/users')
+        .set('Authorization', token)
+      expect(response.statusCode).toBe(200)
+    })
+  })
+
   describe('Delete all users', () => {
     test('should respond with a 200 status code', async () => {
       const response = await request.delete('/api/v1/all-users').send({
